4ec9861cd2f3ab3686f6b3b78735ba858fc671ae
IsaqueS <isaquesalmeida@outlook.com>
2025-05-14 12:57:18 -0300

    "path" command can now accept 0 arguments or 1+ arguments

--- src/path.py
+++ src/path.py
@@ -1,21 +1,26 @@
-import click, os, tomllib, sys
+import click, os, sys
 from typing import Any
 from pathlib import Path
 from .save import settings, CURRENT_PROJECT_FILE_VERSION
 
 @click.command(help="Prints the project path")
 @click.option("-t", "--toml", help="Prints the project file path instead of the project directory path", is_flag=True)
-@click.argument("project", nargs = 1)
-def path(project, toml) -> None:
-    project_file_name = settings["project"]["file-name"]
-    projects_path: Path = Path() / os.path.expanduser("/".join(settings["project"]["video-projects-folder"]))
-    
-    toml_path: Path = projects_path / project / project_file_name
+@click.argument("projects", nargs = -1)
+def path(projects, toml) -> None:
+    if len(projects) == 0:
+        click.echo(os.path.expanduser("/".join(settings["project"]["video-projects-folder"])) + "/") # HACK: will break on windows
+        sys.exit(0)
 
-    if os.path.exists(toml_path):
-        if toml:
-            click.echo(toml_path)
+    for project in projects:            
+        project_file_name = settings["project"]["file-name"]
+        projects_path: Path = Path() / os.path.expanduser("/".join(settings["project"]["video-projects-folder"]))
+        
+        toml_path: Path = projects_path / project / project_file_name
+
+        if os.path.exists(toml_path):
+            if toml:
+                click.echo(str(toml_path))
+            else:
+                click.echo(str(projects_path / project) + "/") # HACK:It will break on windows! 
         else:
-            click.echo(projects_path / project)
-    else:
-        click.secho(f"Project does not exist", fg="red")
+            click.secho(f"Project does not exist", fg="red")
